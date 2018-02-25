ae65a906d1faef70bccc6ca096f3e6187633d810
nicolasbeauvais <nicolasbeauvais1@gmail.com>
2018-02-25 22:12:07 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 #
-# Update and symlinks the dot files.
-#git pull --rebase origin master
 
 #
 # Prezto
@@ -18,10 +16,9 @@ for dotfile in "$HOME"/.dotfiles/prezto/runcoms/*; do
     ln -s "$dotfile" "$HOME/.${dotfile#$HOME/.dotfiles/prezto/runcoms/}"
 done
 
-# Generic config files
+#
+# Config files
+#
 for config in "$HOME"/.dotfiles/.config/*; do
     ln -s "$config" "$HOME"/.config
 done
-
-# Emacs
-ln -s "$HOME"/.dotfiles/.emacs.d "$HOME"/.emacs.d
