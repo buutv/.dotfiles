b8fff202da37bafe9d4e3a3f7609f9348af82471
nicolasbeauvais <nicolasbeauvais1@gmail.com>
2018-02-28 14:46:07 +0100

    Autolaod snippets dir + model snippet

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -5,6 +5,10 @@
 (setq default-directory "~/work")
 
 
+;; Snippets
+;; Snippets
+(setq yas-snippet-dirs '("~/.config/doom/snippets"))
+
 ;; PHP
 (setq flycheck-phpcs-standard "PSR2")
 (setq php-cs-fixer-level-option "~/.config/doom/phpcs-PSR2-custom.xml")
