bb8f93a946654bb71ecdc69ca2cd6db509e1c040
Nicolas Beauvais <nicolasbeauvais1@gmail.com>
2018-06-08 10:00:41 +0200

    Improve load speed using some xrandr checks + fix wallpaper at boot

--- .config/i3/config.sh
+++ .config/i3/config.sh
@@ -5,26 +5,21 @@ ws4="4:  "
 ws5="5:  "
 ws6="6:  "
 
-i3workspaces="$(i3-msg -t get_workspaces)"
-
 # dual-monitor
 if [ `xrandr | grep -c ' connected '` -eq 2 ]; then
     if [ `xrandr | grep HDMI-1 | grep -c ' connected '` -eq 1 ]; then
-        # If not already configured
-        if [ `xrandr | grep HDMI-1 | grep -c 'normal left inverted right x axis y axis'` -ne 1 ]; then
-            # If home QHD screen       
-            if [ `xrandr | grep HDMI-1 | grep -c '708mm'` -eq 1 ]; then
-                xrandr --output eDP-1 --auto --primary --output HDMI-1 --mode 2560x1440_30.00 --right-of eDP-1
-            else
-                xrandr --output eDP-1 --auto --primary --output HDMI-1 --auto --right-of eDP-1
-            fi
+        # If home QHD screen       
+        if [ `xrandr | grep HDMI-1 | grep -c '708mm'` -eq 1 ]; then
+            xrandr --output eDP-1 --auto --primary --output HDMI-1 --mode 2560x1440_30.00 --right-of eDP-1
+        else
+            xrandr --output eDP-1 --auto --primary --output HDMI-1 --auto --right-of eDP-1
         fi
-
+        
         # If not already configured
-        if [ `xrandr | grep eDP-1 | grep 'connected' | grep 'primary' | grep 'normal left' | grep -c '1360x768'` -ne 1 ]; then
+        if [ `xrandr | grep eDP-1 | grep 'connected' | grep 'primary' | grep -c '1360x768'` -ne 1 ]; then
             xrandr --output eDP-1 --mode 1360x768
         fi
-
+        
         i3-msg "workspace $ws2; move workspace to output eDP-1"
         i3-msg "workspace $ws3; move workspace to output HDMI-1"
         i3-msg "workspace $ws4; move workspace to output HDMI-1"
@@ -34,10 +29,10 @@ if [ `xrandr | grep -c ' connected '` -eq 2 ]; then
     fi
 else
     # If not already configured
-    if [ `xrandr | grep eDP-1 | grep 'connected' | grep 'primary' | grep 'normal left' | grep -c '1920x1080'` -ne 1 ]; then
+    if [ `xrandr | grep eDP-1 | grep 'connected' | grep 'primary' | grep -c '1920x1080'` -ne 1 ]; then
         xrandr --output eDP-1 --auto --primary --mode 1920x1080
     fi
-
+    
     i3-msg "workspace $ws2; move workspace to output eDP-1"
     i3-msg "workspace $ws3; move workspace to output eDP-1"
     i3-msg "workspace $ws4; move workspace to output eDP-1"
@@ -45,3 +40,5 @@ else
     i3-msg "workspace $ws6; move workspace to output eDP-1"
     i3-msg "workspace $ws1; move workspace to output eDP-1"
 fi
+
+feh --bg-fill $HOME/wallpaper.jpg --bg-fill $HOME/wallpaper.jpg
